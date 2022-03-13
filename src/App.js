de1c598948cb2c2999843de98c2219fcdec4d489
DoofisS <thevlad2698@gmail.com>
2022-03-13 19:34:23 +0200

    App.js updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import Button from "./Componennts/Button";
+import Player from "./Componennts/Player";
+import Opponent from "./Componennts/Opponent";
+import Container from "./Componennts/Container";
+import PlayerResult from "./Componennts/PlayerResult";
+import OpponentResult from "./Componennts/OpponentResult";
+import LoseWon from "./Componennts/LoseWon";
+import Highscore from "./Componennts/Highscore";
 
 function App() {
+  const [loseWon, setLoseWon] = useState("Your Result");
+  const [highscore, setHighscore] = useState(0);
+  const [playerResult, setPlayerResult] = useState(0);
+  const [opponentResult, setOpponentResult] = useState(0);
+
+  const random = (min, max) => {
+    let player = Math.floor(Math.random() * (max - min)) + min;
+    let opponent = Math.floor(Math.random() * (max - min)) + min;
+    setPlayerResult(player);
+    setOpponentResult(opponent);
+    if (player > opponent) {
+      setHighscore(highscore + 1);
+      setLoseWon("You Won");
+    } else if (player == opponent) {
+      setLoseWon("Draw, try again");
+    } else {
+      setHighscore(0);
+      setLoseWon("You lose");
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoseWon loseWon={loseWon} />
+      <Highscore highscore={highscore} />
+      <Container>
+        <Player />
+        <Opponent />
+      </Container>
+      <Button random={random} />
+      <Container>
+        <PlayerResult playerResult={playerResult} />
+        <OpponentResult opponentResult={opponentResult} />
+      </Container>
     </div>
   );
 }
